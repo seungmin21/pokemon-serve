8e5e8fed90c379ed59b2f1f5979ae7d914a7756b
ohseungmin <ohmin2886@gmail.com>
2023-10-05 17:48:08 +0900

    간단하게 css style 적용

--- mainSubpage.js
+++ mainSubpage.js
@@ -7,7 +7,8 @@ const http = require('http');
 // a태그의 주소 서브는 /sub
 const server = http.createServer(function(request, response) {
   if(request.url === "/") {
-    let doc = `<html><head></head><body><a href="/sub">서브 페이지로</body></html>`
+    let doc = `<html><head><style>div {color : salmon}
+    </style></head><body><div>연어는 영어로 salmon</div><a href="/sub">서브 페이지</a> </body></html>`
     // 맥락이 비슷하게 생김
     // meta http-equiv='Content-Type' content='text/html; charset=utf-8'
     // 인코딩 값을 UTF-8 방식으로 하겠다는 것
@@ -16,7 +17,7 @@ const server = http.createServer(function(request, response) {
     response.end(doc);
   }
   if(request.url === "/sub") {
-    let doc = `<html><head></head><body><a href="/">메인 페이지로</body></html>`
+    let doc = `<html><head></head><body><a href="/">메인 페이지</body></html>`
     response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
     response.end(doc);
   }
